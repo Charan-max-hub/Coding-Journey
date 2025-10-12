f66b5b92841a2f6d0bed77209bf1841e2fa818e3
Charan P <vibhavspark@gmail.com>
2025-10-12 20:47:26 +0530

    Add files via upload

---
+++ SQLQuery2.sql
@@ -0,0 +1,22 @@
+create table person(
+id int not null
+)
+
+alter table persons
+add person_name varchar(10) not null,
+	country_name varchar(10) not null,
+	salary int not null
+
+select * from persons
+
+select * from customers
+
+ insert into persons (id,person_name,country_name,salary)
+
+ select 
+ id,
+ first_name,
+ country,
+ score
+
+ from customers
